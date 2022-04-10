7a3ea3e59b3ec5f14174171f241f1296fd91da54
jlavoieqc <97467718+jlavoieqc@users.noreply.github.com>
2022-04-10 10:37:27 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ ___    _              _              _    _ 
+| . | _| | ___ ._ _ _ | |__ ___  _ _ | |__<_>
+|   |/ . |/ ._>| ' ' || / // . \| | || / /| |
+|_|_|\___|\___.|_|_|_||_\_\\___/`___||_\_\|_|
+                                             
+ By @AdemKouki
+ GitHub : https://github.com/Ademking/InstaPoster
+ Email : me@gmail.com
+ Facebook : https://github.com/Ademking/InstaPoster
+
+ */
+$username = 'superzidol';
+$password = '28715Juju!!!';
+
+$image_description = 'Check this beautiful image lol ...'; 
+$imgurl = 'https://picsum.photos/700/?random';
+$photoFilename = "img/rand.jpg";
